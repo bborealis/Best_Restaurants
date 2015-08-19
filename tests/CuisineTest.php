9f71eb461c0cb62bae35063237312b0070e525c5
Brian Borealis & Jenna Navratil <student@epicodus.com>
2015-08-19 11:29:54 -0700

    add test_getAll and test_deleteAll.

--- tests/CuisineTest.php
+++ tests/CuisineTest.php
@@ -16,7 +16,7 @@
         protected function tearDown() {
             Cuisine::deleteAll();
         }
-        
+
         function test_getName()
         {
             $name = "Chinese";
@@ -36,6 +36,35 @@
             $result = Cuisine::getAll();
 
             $this->assertEquals($test_cuisine,  $result[0]);
+        }
+
+        function test_getAll()
+        {
+            $name = "Chinese";
+            $name2 = "Greek";
+            $test_cuisine = new Cuisine($name);
+            $test_cuisine->save();
+            $test_cuisine2 = new Cuisine($name2);
+            $test_cuisine2->save();
+
+            $result = Cuisine::getAll();
+
+            $this->assertEquals([$test_cuisine, $test_cuisine2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            $name = "Chinese";
+            $name2 = "Greek";
+            $test_category = new Cuisine($name);
+            $test_category->save();
+            $test_category2 = new Cuisine($name2);
+            $test_category2->save();
+
+            Cuisine::deleteAll();
+            $result = Cuisine::getAll();
+
+            $this->assertEquals([], $result);
         }
 
     }
