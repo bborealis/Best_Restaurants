84df67f0d37882423df711d164c3ddc163273f8a
Brian Borealis & Jenna Navratil <student@epicodus.com>
2015-08-20 11:06:10 -0700

    add app.php setup and index, restaurants, and delete twig pages.

--- app/app.php
+++ app/app.php
@@ -1,3 +1,36 @@
 <?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Cuisine.php";
+    require_once __DIR__."/../src/Restaurant.php";
+
+    //Debugger
+    //add symfony debug componet
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $server = 'mysql:host=localhost;dbname=best_restaurants';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->get("/restaurants", function() use ($app) {
+        return $app['twig']->render('restaurants.html.twig', array('restaurants' => Restaurant::getAll()));
+    });
+
+
+
+    return $app;
+
+
 
 ?>
